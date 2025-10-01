0aa7d3f4803047403f891e32cea31fea4ef3e42d
fejlesztok-programozas <fejlesztok@otpfay.hu>
2025-10-01 13:42:06 +0000

    FELTOLTES

---
+++ data/subtitles0.js
@@ -0,0 +1,6 @@
+(function () {
+	const subtitles = {
+			data: "WEBVTT\n\n00:06.219 --> 00:10.800\nCongratulations! You have reached the end\nof the fourth topic! The next topic will cover\n\n00:10.819 --> 00:14.219\neverything you need to know \nabout loans.\n\n00:14.859 --> 00:18.659\nThis includes the different types of loans, \nthe loan application process,\n\n00:19.020 --> 00:22.819\nthe risks associated with loans, and how to reduce these risks.\n\n00:23.699 --> 00:28.020\nRemember, if you need it, the embed dictionary is still at your disposal!\n\n"
+		};
+	window["st_{C0A67928-7211-4F68-9647-E5B5C452E624}"]?.("sndAsset0", subtitles);
+})();
